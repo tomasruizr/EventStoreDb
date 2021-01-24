027db80ca670a72cb8d46a54446fadbb14b80897
Tomás Ruiz <tomasruizr@gmail.com>
2021-01-24 15:26:42 -0300

    [add] initial example building

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <string>
+#include "threadpool/threadpool.h"
 
 using namespace std;
 
+int multiply(int x, int y) {
+    return x * y;
+}
+
 int main() {
-    cout << "Hello World\n";
-    cout << "Input: ";
-    string data;
-    getline(cin, data);
-    cout << "Output: " << data << "\n\n";
+    thread_pool pool(4);
+    std::vector<std::future<int>> futures;
+
+    for (const int &x : { 2, 4, 7, 13 }) {
+        futures.push_back(pool.execute(multiply, x, 2));
+    }
+
+    for (auto &fut : futures) {
+        std::cout << fut.get() << std::endl;
+    }
+
     return 0;
 }
